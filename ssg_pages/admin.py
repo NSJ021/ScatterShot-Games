927ae7b6b82382e0e7dcc2b1a14070ceae28bc67
Nathan Jones <nsjones021@gmail.com>
2024-11-25 22:02:57 +0000

    ssg_pages admin.py, urls.py, Team Model

--- ssg_pages/admin.py
+++ ssg_pages/admin.py
@@ -1,3 +1,66 @@
+"""
+Admin configuration for the Team model.
+
+This module sets up the admin interface for the Team model, 
+including display options, filters, search fields, and integration 
+with the Summernote text editor.
+
+Classes:
+    TeamAdmin(SummernoteModelAdmin): Custom admin interface for the Team model.
+"""
+from django.utils.html import format_html
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Team
+
 
 # Register your models here.
+
+@admin.register(Team)
+class TeamAdmin(SummernoteModelAdmin):
+    """
+    Admin panel configuration for the Team model.
+
+    **Attributes:**
+
+    - `list_display`:
+        Specifies the fields to display in the list view:
+        position, thumbnail, first name, last name, and updated on.
+    - `list_filter`:
+        Specifies the fields to filter the list view by:
+        position and updated on.
+    - `search_fields`:
+        Specifies the fields to include in the search functionality:
+        position, first name, and last name.
+    - `summernote_fields`:
+        Specifies the fields to use the Summernote text editor on: content.
+    - `list_display_links`:
+        Specifies the fields that should be clickable links in the list view:
+        position, thumbnail, first name, and last name.
+    """
+
+    def thumbnail(self, team_member):
+        """
+        Generates an HTML img tag for the team member's profile image.
+
+        Args:
+            team_member (Team): The team member instance.
+
+        Returns:
+            str: An HTML img tag with the profile image URL.
+        """
+        return format_html(
+            f'<img src="{team_member.profile_image.url}" width="50" style="border-radius:50px;" />')
+    thumbnail.short_description = 'Image'
+
+    # List display fields for the Team model
+    list_display = ('position', 'thumbnail', 'first_name',
+                    'last_name', 'updated_on')
+    # List filter fields for the Team model
+    list_filter = ('position', 'updated_on')
+    # List display links for the Team model
+    list_display_links = ('position', 'thumbnail', 'first_name', 'last_name',)
+    # Search fields for the Team model
+    search_fields = ('position', 'first_name', 'last_name',)
+    # Summernote fields for the Team model
+    summernote_fields = ('content',)
