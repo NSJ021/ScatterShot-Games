e294d3d4bb484c44cc2aef89acb2b53fb9ed9489
Nathan Jones <nsjones021@gmail.com>
2024-11-26 11:23:44 +0000

    initial migration, all default migrattions complete, team model migration

--- ssg_config/settings.py
+++ ssg_config/settings.py
@@ -39,7 +39,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1',
+                 '.herokuapp.com']
 
 
 # Application definition
