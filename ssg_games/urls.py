c05ed462bc8722ed5caf157a44033e33e9342961
Nathan Jones <nsjones021@gmail.com>
2024-11-28 13:57:51 +0000

    Add ssg_games model, urls.py

--- ssg_games/urls.py
+++ ssg_games/urls.py
@@ -1,16 +1,18 @@
 """
-URL configuration for the SSG pages application.
+URL configuration for the SSG games application.
 
-This module defines the URL patterns for the pages application,
+This module defines the URL patterns for the games application,
 mapping URLs to views.
 """
 
 from django.urls import path
 from . import views
 
-# Define the URL patterns for the pages application
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about', views.about, name='about'),
-    path('blog', views.blog, name='blog'),
+    path('', views.games, name="games"),
+    path('<slug:slug>/', views.game_detail, name='game_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
