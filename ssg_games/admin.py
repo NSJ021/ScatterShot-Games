516e7305581067b0aacb4821b7557adb59172461
Nathan Jones <nsjones021@gmail.com>
2024-12-06 15:39:26 +0000

    Add Testing section to ReadMe, info to deployment section and validators section

--- ssg_games/admin.py
+++ ssg_games/admin.py
@@ -1,34 +1,37 @@
-from django.contrib import admin
 
 # Register your models here.
 """
 Admin configuration for the Game model.
 
-This module sets up the admin interface for the Game model, 
-including display options, filters, search fields, prepopulated fields, 
+This module sets up the admin interface for the Game model,
+including display options, filters, search fields, prepopulated fields,
 and integration with the Summernote text editor.
 
 Classes:
-    GamesAdmin(SummernoteModelAdmin): Custom admin interface for the Game model.
+    GamesAdmin(SummernoteModelAdmin):
+     Custom admin interface for the Game model.
 
 GamesAdmin:
     list_display:
-        Specifies the fields to display in the list view: game title, 
+        Specifies the fields to display in the list view: game title,
         thumbnail, slug, creator, game brief, and is featured.
     list_filter:
         Specifies the fields to filter the list view by: is featured.
     search_fields:
-        Specifies the fields to include in the search functionality: 
+        Specifies the fields to include in the search functionality:
         game title, game brief, and description.
     list_display_links:
         Specifies the fields that should be clickable links in the list view:
         game title, thumbnail, and game brief.
     list_editable:
-        Specifies the fields that should be editable directly in the list view: is featured.
+        Specifies the fields that should be editable directly in the list view:
+         is featured.
     prepopulated_fields:
-        Specifies the fields to prepopulate based on other fields: slug from game title.
+        Specifies the fields to prepopulate based on other fields:
+         slug from game title.
     summernote_fields:
-        Specifies the fields to use the Summernote text editor on: game brief and description.
+        Specifies the fields to use the Summernote text editor on:
+         game brief and description.
 """
 
 from django.utils.html import format_html
@@ -47,30 +50,36 @@ class GamesAdmin(SummernoteModelAdmin):
     **Attributes:**
 
     - `list_display`:
-        Specifies the fields to display in the list view: game title, 
+        Specifies the fields to display in the list view: game title,
         thumbnail, slug, creator, game brief, and is featured.
     - `list_filter`:
         Specifies the fields to filter the list view by: is featured.
     - `search_fields`:
-        Specifies the fields to include in the search functionality: 
+        Specifies the fields to include in the search functionality:
         game title, game brief, and description.
     - `list_display_links`:
-        Specifies the fields that should be clickable links in the list view: game title, 
+        Specifies the fields that should be clickable links in the list view:
+         game title,
         thumbnail, and game brief.
     - `list_editable`:
-        Specifies the fields that should be editable directly in the list view: is featured.
+        Specifies the fields that should be editable directly in the list view:
+         is featured.
     - `prepopulated_fields`:
-        Specifies the fields to prepopulate based on other fields: slug from game title.
+        Specifies the fields to prepopulate based on other fields:
+         slug from game title.
     - `summernote_fields`:
-        Specifies the fields to use the Summernote text editor on: game brief and description.
+        Specifies the fields to use the Summernote text editor on:
+         game brief and description.
     """
 
     def game_thumbnail(self, game):
         """
-        Custom method to display the game's main image as a thumbnail in the admin list view.
+        Custom method to display the game's main image as a thumbnail
+         in the admin list view.
         """
         return format_html(
-            f'<img src="{game.game_image_main.url}" width="50" style="border-radius:50px;" />')
+            f'<img src="{game.game_image_main.url}"'
+            'width="50" style="border-radius:50px;" />')
     game_thumbnail.short_description = 'Image'
 
     # Fields to display in the admin list view
@@ -116,16 +125,16 @@ class CommentAdmin(SummernoteModelAdmin):
     **Attributes:**
 
     - `list_display`:
-        Specifies the fields to display in the list view: 
+        Specifies the fields to display in the list view:
         game, body, author, approved, and created on.
     - `list_filter`:
-        Specifies the fields to filter the list view by: author, 
+        Specifies the fields to filter the list view by: author,
         created on, and approved.
     - `search_fields`:
-        Specifies the fields to include in the search functionality: 
+        Specifies the fields to include in the search functionality:
         game, body, author, and created on.
     - `list_display_links`:
-        Specifies the fields that should be clickable links in the list view: 
+        Specifies the fields that should be clickable links in the list view:
         post, body, and author.
     """
 
